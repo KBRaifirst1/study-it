fbebcbb40273e6c8b323fc8d22f3bfe5ec23d0a4
KBRaifirst1 <kbraifirst1@gmail.com>
2026-08-10 12:16:01 -0400

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,4 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
-import App from './Lectern.jsx';
-
-createRoot(document.getElementById('root')).render(<App />);
+import Lectern from './Lectern.jsx';
+createRoot(document.getElementById('root')).render(<Lectern />);
